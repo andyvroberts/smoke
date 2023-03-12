16ed20aff267ca542a38db341c75ae0b6160b095
Andy.R <avroberts@outlook.com>
2023-03-12 09:48:08 +0000

    added final dispute file scanner

--- S0142/Common/Constants.cs
+++ S0142/Common/Constants.cs
@@ -13,10 +13,12 @@ namespace S0142.Common
         internal const string ConfigFinalReconRK = "S0142-RF";
         internal const string ConfigInterimInitRK = "S0142-II";
         internal const string ConfigFirstReconRK = "S0142-R1";
+        internal const string ConfigFinalDisputeRK = "S0142-DF";
 
         // SAA run types
         internal const string FinalReconciliation = "RF";
         internal const string InterimInitial = "II";
         internal const string FirstReconciliation = "R1";
+        internal const string FinalDispute = "DF";
     }
 }
---
+++ S0142/ScannerDF.cs
@@ -0,0 +1,74 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Azure.Data.Tables;
+using Azure;
+using System.Linq;
+
+namespace S0142
+{
+    using S0142.Common;
+    using S0142.Models;
+    using S0142.Services;
+
+    public static class ScannerDF
+    {
+        [FunctionName("S0142-DF-Scanner")]
+        public static async Task Scan([TimerTrigger("*/3 * * * *", RunOnStartup = true)] TimerInfo scanTimer,
+        [Table("AcquisitionConfig", Constants.ConfigPK, Constants.ConfigFinalDisputeRK, Connection = "EnergyDataConfigStore")] ConfigTable cd,
+        [Table("S0142Files", Connection = "EnergyDataConfigStore")] TableClient filesTab,
+        ILogger log)
+        {
+            log.LogInformation("C# Timer trigger function processed a request.");
+            var nextDate = cd.Latest.AddDays(1);
+
+            if (nextDate.Date < DateTime.Now.Date)
+            {
+                string urlDate = $"{nextDate.Year}-{nextDate.Month:00}-{nextDate.Day:00}";
+                log.LogInformation($"Run Date = {urlDate}");
+
+                var fileEntities = await ListFiles.FilesForRunDate(Environment.GetEnvironmentVariable("BmrsApiKey"), urlDate);
+
+                if (fileEntities.Any())
+                {
+                    foreach (var fileEntity in fileEntities)
+                    {
+                        if (fileEntity.RowKey == Constants.FinalDispute)
+                        {
+                            Pageable<FileListTable> qRes = filesTab.Query<FileListTable>(filter: $"PartitionKey eq '{fileEntity.PartitionKey}' and RowKey eq '{fileEntity.RowKey}'");
+
+                            if (!qRes.Any())
+                            {
+                                await Lake.AddToLake(
+                                    Environment.GetEnvironmentVariable("BmrsApiKey"),
+                                    Environment.GetEnvironmentVariable("EnergyDataLake"),
+                                    fileEntity.PartitionKey,
+                                    Environment.GetEnvironmentVariable("Container"),
+                                    fileEntity.FileName,
+                                    log);
+                                await filesTab.UpsertEntityAsync(fileEntity);
+                            }
+                            else
+                            {
+                                log.LogWarning($"File for {fileEntity.PartitionKey}-{fileEntity.RowKey} already exists in S0142 Files table.");
+                                log.LogWarning($"Skipping download for {fileEntity.FileName}");
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    log.LogWarning($"Empty file list for date {urlDate}.");
+                }
+
+                cd.Latest = nextDate;
+                log.LogInformation($"Updated Binding Date for completion of = {nextDate}");
+            }
+            else
+            {
+                log.LogInformation($"No Execution - {nextDate} is greater than {DateTime.Now.Date}");
+            }
+        }
+    }
+}
